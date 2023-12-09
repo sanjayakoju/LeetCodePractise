0ba92c04c93f45b46aa16120866051d4612a3f60
sanjaya.koju <sanjayakoju42@gmail.com>
2023-12-09 12:29:44 -0800

    Changes consecutive logic

--- src/assessment/collabera/aws/ConsecutiveNumbersGame.java
+++ src/assessment/collabera/aws/ConsecutiveNumbersGame.java
@@ -16,9 +16,13 @@ import java.util.List;
  * Round 2: {34 100800 16200000 900000 100}
  * Round 3: {3427200 100800 16200000 90000000 100}
  * No. of rounds: 3
+ * List: 1, 333, 222, 2
+ * Round 1
+ * No. of rounds: 1
  */
 public class ConsecutiveNumbersGame {
-
+//1,333,222,22
+    //1-
     public static int solve(List<Integer> arr) {
         int rounds = 0;
         boolean changed = true;
@@ -27,10 +31,11 @@ public class ConsecutiveNumbersGame {
             changed = false;
             List<Integer> tempArr = new ArrayList<>(arr);
 
-            for (int i=0;i<tempArr.size() -1 ;i++) {
+            for (int i=tempArr.size()-2;i>=0;i--) {
                 int product = tempArr.get(i) * tempArr.get(i+1);
                 if (product % 3 == 0) {
                     arr.set(i, product);
+                    arr.remove(i+1);
                     changed = true;
                 }
             }
@@ -47,5 +52,9 @@ public class ConsecutiveNumbersGame {
         List<Integer> list = new ArrayList<>(Arrays.asList(34, 56, 20, 90, 100));
 
         System.out.println("No. of rounds: "+solve(list));
+
+        List<Integer> list2 = new ArrayList<>(Arrays.asList(1, 333, 222, 2));
+
+        System.out.println("No. of rounds: "+solve(list2));
     }
 }
